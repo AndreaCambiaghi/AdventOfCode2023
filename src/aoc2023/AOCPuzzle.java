997f26ac084ff6b862eaf67b212a41204ad93a19
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2023-11-30 22:11:35 +0100

    first commit

---
+++ src/aoc2023/AOCPuzzle.java
@@ -0,0 +1,59 @@
+package aoc2019;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AOCPuzzle {
+
+    public AOCPuzzle(String day) {
+        File file = new File("in/day" + day + ".txt");
+        if(!file.exists()) {
+            solve(new ArrayList<>());
+            return;
+        }//if
+
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+        } catch(FileNotFoundException e) {
+            System.err.println("File not found!!");
+            solve(new ArrayList<>());
+            return;
+        }//catch
+
+        List<String> inputLines = new ArrayList<>();
+        try {
+            String line;
+            while((line = reader.readLine()) != null)
+                inputLines.add(line);
+
+            reader.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }//catch
+
+        solve(inputLines);
+    }//AOCPuzzle
+
+    abstract void solve(List<String> input);
+
+    public List<Integer> convertToInts(List<String> input) {
+        List<Integer> ints = new ArrayList<>();
+        for(String s : input)
+            ints.add(Integer.parseInt(s));
+        return ints;
+    }//convertToInts
+
+    public List<Long> convertToLongs(List<String> input) {
+        List<Long> ints = new ArrayList<>();
+        for(String s : input)
+            ints.add(Long.parseLong(s));
+        return ints;
+    }//convertToLongs
+
+}//class
---
+++ src/aoc2023/Day01.java
@@ -0,0 +1,14 @@
+package aoc2019;
+
+import java.util.List;
+
+public class Day01 extends AOCPuzzle {
+
+    public Day01() { super("1"); }
+
+    @Override
+    void solve(List<String> input) {
+
+    }//solve
+
+}//class
