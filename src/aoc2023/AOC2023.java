ac6e9cfaba079c92a15c01cf7c1ce5544c6d2fa0
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2023-12-02 00:39:53 +0100

    merge with main

--- src/aoc2023/AOC2023.java
+++ src/aoc2023/AOC2023.java
@@ -2,6 +2,7 @@ package aoc2023;
 
 public class AOC2023 {
     public static void main(String[] args) {
-        new Day01();
+        //new Day01();
+        new Day02();
     }//main
 }//class
---
+++ src/aoc2023/Day02.java
@@ -0,0 +1,13 @@
+package aoc2023;
+
+import java.util.List;
+
+public class Day02 extends AOCUtils {
+    public Day02() { super("2"); }
+
+    @Override
+    void solve(List<String> input) {
+
+    }//solve
+
+}//class
