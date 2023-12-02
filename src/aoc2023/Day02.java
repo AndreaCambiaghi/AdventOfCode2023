441941e4b5f579ddc926e1b93326e4e3dcb47d56
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2023-12-02 17:37:26 +0100

    day2

--- src/aoc2023/Day02.java
+++ src/aoc2023/Day02.java
@@ -1,12 +1,61 @@
 package aoc2023;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day02 extends AOCUtils {
     public Day02() { super("2"); }
 
+    // 12 red cubes, 13 green cubes, and 14 blue cubes
     @Override
     void solve(List<String> input) {
+
+        Map<String, Integer> maxValid = Map.of(
+                "red", 12,
+                "green", 13,
+                "blue", 14
+        );
+
+        int sumId = 0;
+        int power = 0;
+        for(String row : input) {
+
+            String[] game = row.split(": ");
+            int gameNumber = Integer.parseInt(game[0].split(" ")[1]);
+            String[] sets = game[1].split("; ");
+
+            Map<String, Integer> maxSetColor = new HashMap<>(Map.of(
+                    "red", 0,
+                    "green", 0,
+                    "blue", 0
+            ));
+
+            for(String set : sets) {
+                String[] cubes = set.split(", ");
+                for (String cube : cubes) {
+                    int bag = Integer.parseInt(cube.split(" ")[0]);
+                    String color = cube.split(" ")[1];
+                    maxSetColor.put(color, Integer.max(maxSetColor.get(color), bag));
+                }//for
+            }//for
+
+            int gamePower = 1;
+            for(String key : maxSetColor.keySet())
+                gamePower *= maxSetColor.get(key);
+
+            boolean valid = true;
+            for(String key : maxSetColor.keySet())
+                if(maxSetColor.get(key) > maxValid.get(key))
+                    valid = false;
+
+            if(valid)
+                sumId += gameNumber;
+            power += gamePower;
+        }//for
+
+        solution(sumId);
+        solution(power);
 
     }//solve
 
