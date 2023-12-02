bf469c81dde473d82b7f422000311e82ef76cb3e
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2023-12-02 10:20:51 +0100

    day2 - part2

--- src/aoc2023/Day02.java
+++ src/aoc2023/Day02.java
@@ -10,18 +10,23 @@ public class Day02 extends AOCUtils {
     // 12 red cubes, 13 green cubes, and 14 blue cubes
     @Override
     void solve(List<String> input) {
-        Map<String, Integer> max = Map.of(
+        Map<String, Integer> maxValid = Map.of(
                 "red", 12,
                 "green", 13,
                 "blue", 14
         );
 
         int sumId = 0;
+        int power = 0;
         for(String row : input) {
             String[] game = row.split(": ");
             int gameNumber = Integer.parseInt(game[0].split(" ")[1]);
             String[] sets = game[1].split("; ");
             Boolean valid = true;
+            Map<String, Integer> maxSetColor = new HashMap<>();
+            maxSetColor.put("red", 0);
+            maxSetColor.put("green", 0);
+            maxSetColor.put("blue", 0);
             for(String set : sets) {
                 String[] cubes = set.split(", ");
                 Map<String, Integer> countColor = new HashMap<>();
@@ -32,16 +37,23 @@ public class Day02 extends AOCUtils {
                     Integer bag = Integer.valueOf(cube.split(" ")[0]);
                     String color = cube.split(" ")[1];
                     countColor.put(color, countColor.get(color) +  bag);
+                    if(maxSetColor.get(color) < bag)
+                        maxSetColor.put(color, bag);
                 }//for
                 for(String colorKey : countColor.keySet()) {
-                    if(countColor.get(colorKey) > max.get(colorKey))
+                    if(countColor.get(colorKey) > maxValid.get(colorKey))
                         valid = false;
                 }//for
             }//for
+            int gamePower = 1;
+            for(String key : maxSetColor.keySet())
+                gamePower *= maxSetColor.get(key);
             if(valid)
                 sumId += gameNumber;
+            power += gamePower;
         }//for
         solution(sumId);
+        solution(power);
     }//solve
 
 }//class
