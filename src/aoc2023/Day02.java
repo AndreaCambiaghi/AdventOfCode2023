c326f13a84501ed856b9339dc5bbdbeeb17618a9
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2023-12-02 11:25:50 +0100

    day2 - refactoring

--- src/aoc2023/Day02.java
+++ src/aoc2023/Day02.java
@@ -10,6 +10,7 @@ public class Day02 extends AOCUtils {
     // 12 red cubes, 13 green cubes, and 14 blue cubes
     @Override
     void solve(List<String> input) {
+
         Map<String, Integer> maxValid = Map.of(
                 "red", 12,
                 "green", 13,
@@ -19,41 +20,43 @@ public class Day02 extends AOCUtils {
         int sumId = 0;
         int power = 0;
         for(String row : input) {
+
             String[] game = row.split(": ");
             int gameNumber = Integer.parseInt(game[0].split(" ")[1]);
             String[] sets = game[1].split("; ");
-            Boolean valid = true;
-            Map<String, Integer> maxSetColor = new HashMap<>();
-            maxSetColor.put("red", 0);
-            maxSetColor.put("green", 0);
-            maxSetColor.put("blue", 0);
+
+            Map<String, Integer> maxSetColor = new HashMap<>(Map.of(
+                    "red", 0,
+                    "green", 0,
+                    "blue", 0
+            ));
+
             for(String set : sets) {
                 String[] cubes = set.split(", ");
-                Map<String, Integer> countColor = new HashMap<>();
-                countColor.put("red", 0);
-                countColor.put("green", 0);
-                countColor.put("blue", 0);
                 for (String cube : cubes) {
-                    Integer bag = Integer.valueOf(cube.split(" ")[0]);
+                    int bag = Integer.parseInt(cube.split(" ")[0]);
                     String color = cube.split(" ")[1];
-                    countColor.put(color, countColor.get(color) +  bag);
-                    if(maxSetColor.get(color) < bag)
-                        maxSetColor.put(color, bag);
-                }//for
-                for(String colorKey : countColor.keySet()) {
-                    if(countColor.get(colorKey) > maxValid.get(colorKey))
-                        valid = false;
+                    maxSetColor.put(color, Integer.max(maxSetColor.get(color), bag));
                 }//for
             }//for
+
             int gamePower = 1;
             for(String key : maxSetColor.keySet())
                 gamePower *= maxSetColor.get(key);
+
+            boolean valid = true;
+            for(String key : maxSetColor.keySet())
+                if(maxSetColor.get(key) > maxValid.get(key))
+                    valid = false;
+
             if(valid)
                 sumId += gameNumber;
             power += gamePower;
         }//for
+
         solution(sumId);
         solution(power);
+
     }//solve
 
 }//class
