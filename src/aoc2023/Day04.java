b01f226db8f182dbd93310616cb76033c1fd8f4b
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2023-12-04 11:22:52 +0100

    day4 - part1

--- src/aoc2023/Day04.java
+++ src/aoc2023/Day04.java
@@ -7,6 +7,13 @@ public class Day04 extends AOCUtils {
 
     @Override
     void solve(List<String> input) {
-
+        int points = 0;
+        for(String card : input) {
+            List<Integer> winnerNumber = convertToInts(List.of(card.split(":\\s+")[1].split(" \\|\\s+")[0].split("\\s+")));
+            List<Integer> myNumber = convertToInts(List.of(card.split(":\\s+")[1].split(" \\|\\s+")[1].split("\\s+")));
+            int diff = (int)winnerNumber.stream().filter(myNumber::contains).count();
+            points += (int) Math.pow(2.0, (diff-1) );
+        }//for
+        solution(points);
     }//solve
 }//class
