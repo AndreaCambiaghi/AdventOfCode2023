40b0600bf34b62852c26f9b58667e3a37f69809d
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2023-12-05 22:20:35 +0100

    day5 - part1 - refactoring with long

--- src/aoc2023/Day05.java
+++ src/aoc2023/Day05.java
@@ -9,54 +9,52 @@ public class Day05 extends AOCUtils {
     @Override
     void solve(List<String> input) {
 
-        Map<BigInteger, BigInteger> seeds = new LinkedHashMap<>();
+        List<Long> seeds = new ArrayList<>();
         for (String seed : input.get(0).split(": ")[1].split(" "))
-            seeds.put(new BigInteger(seed), new BigInteger(seed));
+            seeds.add(Long.valueOf(seed));
         input.remove(0);
         input.remove(1);
 
-        for (int i = 0; i < input.size(); ) {
-            List<BigInteger> excluded = new ArrayList<>();
-            while (!input.get(i).contains("map")) {
+        long min = Long.MAX_VALUE;
 
-                if (input.get(i).isBlank()) {
-                    i++;
-                    continue;
-                }//if
+        for (Long key : seeds) {
+            Long curr = key;
+            for (int i = 0; i < input.size(); ) {
+                List<Long> excluded = new ArrayList<>();
+                while (!input.get(i).contains("map")) {
 
-                List<BigInteger> rowNumber = convertToBigIntegers(List.of(input.get(i).split(" ")));
-
-                for (BigInteger key : seeds.keySet()) {
-                    if (
-                        !excluded.contains(key) &&
-                        rowNumber.get(1).compareTo(seeds.get(key)) <= 0 &&
-                        rowNumber.get(1).add(rowNumber.get(2)).compareTo(seeds.get(key)) > 0
-                    ) {
-                        excluded.add(key);
-                        seeds.put(key, seeds.get(key).add(rowNumber.get(0).subtract(rowNumber.get(1))));
+                    if (input.get(i).isBlank()) {
+                        i++;
+                        continue;
                     }//if
 
-                }//for
+                    List<Long> rowNumber = convertToLongs(List.of(input.get(i).split(" ")));
+
+                    if (
+                            !excluded.contains(key) &&
+                                    rowNumber.get(1) <= curr &&
+                                    rowNumber.get(1) + rowNumber.get(2) > curr
+                    ) {
+                        excluded.add(key);
+                        curr = curr + rowNumber.get(0) - rowNumber.get(1);
+                    }//if
+
+                    i++;
+                    if (i >= input.size())
+                        break;
+
+                }//while
 
                 i++;
-                if (i >= input.size())
-                    break;
 
-            }//while
-
-            i++;
+            }//for
+            if (min > curr)
+                min = curr;
 
         }//for
 
-        solution(seeds.values().stream().min(BigInteger::compareTo).orElse(BigInteger.ZERO).toString());
+        solution(min);
 
     }//solve
-
-    private static List<BigInteger> convertToBigIntegers(List<String> strings) {
-        List<BigInteger> bigIntegers = new ArrayList<>();
-        for (String str : strings)
-            bigIntegers.add(new BigInteger(str));
-        return bigIntegers;
-    }//convertToBigIntegers
 
 }//class
