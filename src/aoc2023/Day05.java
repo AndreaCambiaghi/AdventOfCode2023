41b070ed9c2a5f2f012ae4796bdc69bd8d819e1f
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2023-12-06 00:51:37 +0100

    day5 - part1 - refactoring

--- src/aoc2023/Day05.java
+++ src/aoc2023/Day05.java
@@ -2,16 +2,16 @@ package aoc2023;
 
 import java.math.BigInteger;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day05 extends AOCUtils {
     public Day05() { super("5"); }
 
+    
     @Override
     void solve(List<String> input) {
 
-        List<Long> seeds = new ArrayList<>();
-        for (String seed : input.get(0).split(": ")[1].split(" "))
-            seeds.add(Long.valueOf(seed));
+        List<Long> seeds = Arrays.stream(input.get(0).split(": ")[1].split(" ")).map(Long::valueOf).toList();
         input.remove(0);
         input.remove(1);
 
@@ -31,9 +31,9 @@ public class Day05 extends AOCUtils {
                     List<Long> rowNumber = convertToLongs(List.of(input.get(i).split(" ")));
 
                     if (
-                            !excluded.contains(key) &&
-                                    rowNumber.get(1) <= curr &&
-                                    rowNumber.get(1) + rowNumber.get(2) > curr
+                    !excluded.contains(key) &&
+                    rowNumber.get(1) <= curr &&
+                    rowNumber.get(1) + rowNumber.get(2) > curr
                     ) {
                         excluded.add(key);
                         curr = curr + rowNumber.get(0) - rowNumber.get(1);
