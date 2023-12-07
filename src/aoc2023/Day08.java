4f4b3b044915e3950ca5bdfff0239f0a6f35970a
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2023-12-07 18:55:12 +0100

    day8 - init

---
+++ src/aoc2023/Day08.java
@@ -0,0 +1,13 @@
+package aoc2023;
+
+import java.util.List;
+
+public class Day08 extends AOCUtils {
+    public Day08() { super("8"); }
+
+    @Override
+    void solve(List<String> input) {
+
+    }//solve
+
+}//class
