478f797795c6a0a94a71a1d961b8c6e5a64bf103
AndreaCambiaghi <andrealuigi.cambiaghi@studenti.unimi.it>
2023-12-08 12:13:43 +0100

    day8 - part1

--- src/aoc2023/Day08.java
+++ src/aoc2023/Day08.java
@@ -1,13 +1,40 @@
 package aoc2023;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day08 extends AOCUtils {
     public Day08() { super("8"); }
 
+    private record Node(String l, String r){}
+
     @Override
     void solve(List<String> input) {
 
+        String cmd = input.remove(0);
+        input.remove(0);
+        Map<String, Node> nodes = new HashMap<>();
+        for(String row : input) {
+            row = row.replaceAll("[=,()]", "");
+            String[] node = row.split("\\s+");
+            nodes.put(node[0], new Node(node[1], node[2]));
+        }//for
+        String curr = "AAA";
+        int idx = 0;
+        while (!curr.equals("ZZZ")) {
+            char lf = cmd.charAt(idx % cmd.length());
+            if(lf == 'L')
+                curr = nodes.get(curr).l;
+            else if(lf == 'R')
+                curr = nodes.get(curr).r;
+            idx++;
+        }//while
+
+        solution(idx);
+
     }//solve
+
 
 }//class
